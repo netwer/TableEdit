830699ebc81ca8253ee635c5648b09c93004dca2
netwer <art9207@gmail.com>
2017-02-12 16:06:52 +0300

    routing

--- TableEdit/src/TableEdit/wwwroot/Scripts/app.js
+++ TableEdit/src/TableEdit/wwwroot/Scripts/app.js
@@ -1,5 +1,13 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('tableEditApp', []);
+    angular.module('tableEditApp', ['ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {        
+        $routeProvider.when('/orders',
+        {
+            templateUrl: 'index.html',
+            controller: 'TableEditController'
+        });
+        $routeProvider.otherwise({ redirectTo: '/orders' });
+    });
 })();
