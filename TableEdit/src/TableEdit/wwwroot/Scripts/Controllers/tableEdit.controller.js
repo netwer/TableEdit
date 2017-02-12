1c2071d9057292271506ec9389308b702e057c66
netwer <art9207@gmail.com>
2017-02-12 13:57:24 +0300

    Client side architecture created

---
+++ TableEdit/src/TableEdit/wwwroot/Scripts/Controllers/tableEdit.controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('tableEditApp')
+        .controller('TableEditController', TableEditController);
+
+    TableEditController.$inject = ['$scope', 'TableEditService'];
+
+    function TableEditController($scope, TableEditService) {
+        var vm = this;
+        vm.orders = null;
+
+        TableEditService.getOrders().then(function (result) {
+            $scope.Orders = result;
+        });
+    }
+})();
---
+++ TableEdit/src/TableEdit/wwwroot/Scripts/Services/tableEdit.service.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('tableEditApp')
+        .factory('TableEditService', TableEditService);
+
+    TableEditService.$inject = ['$http'];
+
+    function TableEditService($http) {
+        var service = {
+            getOrders: getOrders
+        };
+
+        return service;
+
+        function getOrders() {
+            return $http.get('/api/orders').then(function (response) {
+                return response.data;
+            }, function (x) {
+                console.log(x);
+            });
+        }
+    }
+})();
