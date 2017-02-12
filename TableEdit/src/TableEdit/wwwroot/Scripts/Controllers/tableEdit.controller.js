7262fa24a9a2f8eb0246123e568ac8a07e167456
netwer <art9207@gmail.com>
2017-02-12 18:41:35 +0300

    Added show/hide column

--- TableEdit/src/TableEdit/wwwroot/Scripts/Controllers/tableEdit.controller.js
+++ TableEdit/src/TableEdit/wwwroot/Scripts/Controllers/tableEdit.controller.js
@@ -7,11 +7,29 @@
     TableEditController.$inject = ['$scope', 'TableEditService'];
 
     function TableEditController($scope, TableEditService) {
-        var vm = this;
-        vm.orders = null;
-
+        
         TableEditService.getOrders().then(function (result) {
             $scope.Orders = result;
+            $scope.Settings = {
+                HideColumn: {
+                    Number: false,
+                    Id: false,
+                    Name: false,
+                    Description: false,
+                    Count: false,
+                    Price: false,
+                    Discount: false,
+                    PriceWithDiscount: false,
+                    OrderDate: false,
+                    DeliveryDaysCount: false,
+                    DeliveryDate: false,
+                }
+                
+            };
         });
+
+        $scope.hideColumn = function ($event, column) {
+            $scope.Settings.HideColumn[column] = $event.target.checked;
+        }
     }
 })();
