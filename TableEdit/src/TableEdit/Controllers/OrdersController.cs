5ea89f109a43a3d85332d25adbcf961fb218d37c
netwer <art9207@gmail.com>
2017-02-19 15:49:06 +0300

    Clean up code

--- TableEdit/src/TableEdit/Controllers/OrdersController.cs
+++ TableEdit/src/TableEdit/Controllers/OrdersController.cs
@@ -19,31 +19,6 @@ namespace TableEdit.Controllers
         public IEnumerable<OrderDto> Get()
         {
             return _orderService.GetOrders();
-        }
-
-        // GET api/orders/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/orders
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/orders/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/orders/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        }        
     }
 }
